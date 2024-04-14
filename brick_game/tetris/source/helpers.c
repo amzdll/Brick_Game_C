de308fb567774ee7c7b569a5421db789faad3a9a
glenpoin <develop.frei@gmail.com>
2024-04-14 21:27:21 +0300

    is_row_complete func written

--- brick_game/tetris/source/helpers.c
+++ brick_game/tetris/source/helpers.c
@@ -7,12 +7,11 @@ bool is_collision(game_board_t game_board, block_t block) {
   }
   for (int i = 0; i < block.height; ++i) {
     for (int j = 0; j < block.width; ++j) {
-      if (block.field[i][j] &&
-          !block.field[i + 1][j]
-//          !block[i][j] &&/
-//          game_board.field[block.y + i][block.x + j] &&
-//          game_board.field[block.y + i + 1][block.x + j]
-          ) {
+      if (block.field[i][j] && !block.field[i + 1][j]
+          //          !block[i][j] &&/
+          //          game_board.field[block.y + i][block.x + j] &&
+          //          game_board.field[block.y + i + 1][block.x + j]
+      ) {
         return true;
       }
     }
@@ -23,4 +22,11 @@ bool is_collision(game_board_t game_board, block_t block) {
 
 bool is_game_over(game_board_t game_board, block_t block) {
   return is_collision(game_board, block) && block.y == 0;
+}
+
+int compare(const void* a, const void* b) { return (*(int*)a - *(int*)b); }
+
+bool is_row_complete(int* row) {
+  int key = 0;
+  return !(int*)bsearch(&key, row, WIDTH, sizeof(int), compare);
 }
--- brick_game/tetris/source/main.c
+++ brick_game/tetris/source/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <wchar.h>
 
 #include "defines.h"
 #include "game_entities.h"
@@ -7,50 +9,80 @@
 
 int main() {
   game_board_t game_board = initialize_game_board();
-  block_t* blocks = initialize_blocks();
-  // 1 3
-//  block_t current_block = create_block(blocks);
-//  while (!is_game_over(game_board, current_block)) {
-//    while (!is_collision(game_board, current_block)) {
-//      down_move(&game_board, &current_block);
-//    }
-//    //      ++n;
-//    current_block = create_block(blocks);
-//  }
-  block_t current_block = blocks[0];
-  current_block.x = (int)(WIDTH - current_block.width) / 2;
-  current_block.y = 19;
-  place_block(&game_board, current_block);
-  block_t temp = blocks[4];
-  temp.x = (int)(WIDTH - temp.width) / 2;
-  temp.y = 17;
-  place_block(&game_board, temp);
-  if (!is_collision(game_board, temp)) {
-    down_move(&game_board, &temp);
-  }
 
-//  printf("%s\n", is_collision(game_board, temp) ? "true" : "false");
+
+  wmemset(game_board.field[0], 1, WIDTH * sizeof(int));
 
 
 
-  printf(" \t");
+  game_board.field[0][0] = 0;
   for (int i = 0; i < WIDTH; ++i) {
-    printf("%d ", i);
+    printf("%d\t", game_board.field[0][i]);
   }
-  printf(" \n");
-  for (size_t i = 0; i < HEIGHT; ++i) {
-    printf("%zu\t", i);
-    for (size_t j = 0; j < WIDTH; ++j) {
-      printf("%d ", game_board.field[i][j]);
-    }
-    printf("\n");
+  printf("\n");
+  if (is_row_complete(game_board.field[0])) {
+    printf("is full\n");
+  } else {
+    printf("isn't full\n");
   }
 
-  free_int_two_dimensional_array(NULL, 0);
-  //  free_blocks(blocks);
+  game_board.field[0][0] = 1;
+  game_board.field[0][1] = 2;
+  game_board.field[0][2] = 3;
+  game_board.field[0][3] = 4;
+  game_board.field[0][4] = 5;
+
+  for (int i = 0; i < WIDTH; ++i) {
+    printf("%d\t", game_board.field[0][i]);
+  }
+  printf("\n");
+
+  if (is_row_complete(game_board.field[0])) {
+    printf("is full\n");
+  } else {
+    printf("isn't full\n");
+  }
+  //  block_t* blocks = initialize_blocks();
+  // 1 3
+  //      //  block_t current_block = create_block(blocks);
+  //      //  while (!is_game_over(game_board, current_block)) {
+  //      //    while (!is_collision(game_board, current_block)) {
+  //      //      down_move(&game_board, &current_block);
+  //      //    }
+  //      //    //      ++n;
+  //      //    current_block = create_block(blocks);
+  //      //  }
+  //      block_t current_block = blocks[0];
+  //  current_block.x = (int)(WIDTH - current_block.width) / 2;
+  //  current_block.y = 19;
+  //  place_block(&game_board, current_block);
+  //  block_t temp = blocks[4];
+  //  temp.x = (int)(WIDTH - temp.width) / 2;
+  //  temp.y = 17;
+  //  place_block(&game_board, temp);
+  //  if (!is_collision(game_board, temp)) {
+  //    down_move(&game_board, &temp);
+  //  }
+  //
+  //  //  printf("%s\n", is_collision(game_board, temp) ? "true" : "false");
+  //
+  //  printf(" \t");
+  //  for (int i = 0; i < WIDTH; ++i) {
+  //    printf("%d ", i);
+  //  }
+  //  printf(" \n");
+  //  for (size_t i = 0; i < HEIGHT; ++i) {
+  //    printf("%zu\t", i);
+  //    for (size_t j = 0; j < WIDTH; ++j) {
+  //      printf("%d ", game_board.field[i][j]);
+  //    }
+  //    printf("\n");
+  //  }
+  //
+  //  free_int_two_dimensional_array(NULL, 0);
+  //  //  free_blocks(blocks);
   return 0;
 }
-
 
 /*
         0 1 2 3 4 5 6 7 8 9
